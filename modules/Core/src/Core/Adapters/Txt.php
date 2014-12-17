7b96dbfa02f91069e8fe6dbc98276e9dea4e4394
Antonio Blasco Cerrada <814ck.c0d3@gmail.com>
2014-12-17 23:51:27 +0100

    Complete last functions/methods

--- modules/Core/src/Core/Adapters/Txt.php
+++ modules/Core/src/Core/Adapters/Txt.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Core\Adapters;
 use Core\Application\Application;
 
@@ -10,7 +11,6 @@ Class Txt implements AdapterInterface, TxtInterface
         $config = Application::getConfig();
         $this->setFilename($_SERVER['DOCUMENT_ROOT']."/".$config['filename']);
     }
-
     public function setFilename($filename) 
     {
         $this->filename = $filename;
@@ -29,38 +29,49 @@ Class Txt implements AdapterInterface, TxtInterface
     }
     
     public function fetch($id) 
-
     {
-        
+        $data = file_get_contents($this->filename);
+        $data = explode("\n", $data);
+        $user = $data[$id];
+        $user = explode("|", $usuario);
+        return $user;
+        break;
     }
     
-
 	public function delete($id)
 	{
-		
+	    $data = file_get_contents($this->filename);
+	    $data = explode("\n", $data);
+	    unset($data[$id]);
+	    $users = implode("\n", $data);
+	    return file_put_contents($this->filename, $users);
+	    break;
 	}
 	
 	public function insert($data)
 	{
-		foreach($filter as $key => $value)
-      	{
-                //         if(is_array($value))
-                    //             $value=implode(',', $value);
-                    //         $data[$key]=$value;
-                    //     }
-            //     $data[]=$imagename;
-            //     $data = implode('|', $data);
-            $filename = 'usuarios.txt';
-            return file_put_contents($_SERVER['DOCUMENT_ROOT']."/".$filename,
-                                        $data."\n",
-                                        FILE_APPEND);
-			}
+	    foreach($data as $key => $value)
+        {
+            return file_put_contents($this->filename, $data."\n", FILE_APPEND);
+        }
 	}
 	
 	public function update($id,$data)
 	{
-		
+	    $users = file_get_contents($this->filename);
+	    $users = explode("\n", $users);
+	    foreach($data as $key => $value)
+	    {
+	        if(is_array($value))
+	            $value=implode(',', $value);
+	        $data[$key]=$value;
+	    }
+	    $data = implode('|', $data);
+	    $users[$data['id']] = $data;
+	    $users = implode("\n", $users);
+	    // Escribir todo el array al fichero
+	    return file_put_contents($this->filename, $users);
+	     
 	}
     
 }
-
